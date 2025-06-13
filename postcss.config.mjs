d24b9c025e762184b930feffdd3a4654bd38eb44
Aarya Joshi <121683053+JoshiAarya@users.noreply.github.com>
2025-06-13 09:56:33 +0530

    vercel fix

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,2 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
-}
+const config = { plugins: { "@tailwindcss/postcss": {} } };
+export default config;
